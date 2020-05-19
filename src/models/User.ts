65eb74303a2074bc9c7140d015867f70f02324cc
Victor Espaillat <vicander_99@hotmail.com>
2020-05-19 17:32:25 -0400

    167. Setting triggers 'change' event

--- src/models/User.ts
+++ src/models/User.ts
@@ -38,4 +38,9 @@ export class User {
     get get() {
         return this.attributes.get;
     }
+
+    set(update: UserProps): void {
+        this.attributes.set(update);
+        this.events.trigger("change");
+    }
 }

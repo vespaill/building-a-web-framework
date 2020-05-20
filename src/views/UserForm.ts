7fd115960d308f6a6178298c80d8dbdeea5c06bf
Victor Espaillat <vicander_99@hotmail.com>
2020-05-20 08:57:47 -0400

    185-186. Defining evnts map & binding evnt handlers

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -4,6 +4,21 @@ export class UserForm {
      */
     constructor(public parent: Element) {}
     /**
+     *
+     */
+    eventsMap(): { [key: string]: () => void } {
+        return {
+            "click:button": this.onButtonClick,
+            "mouseenter:h1": this.onHeaderHover
+        };
+    }
+    onButtonClick(): void {
+        console.log("button was clicked");
+    }
+    onHeaderHover(): void {
+        console.log("H1 was hovered over");
+    }
+    /**
      * Returns a string that contains a UserForm HTML template.
      */
     template(): string {
@@ -11,14 +26,31 @@ export class UserForm {
             <div>
                 <h1>User Form</h1>
                 <input />
+                <button>Click Me</button>
             </div>
         `;
+    }
+    /**
+     * @param fragment reference to HTML we're trying to insert into the DOM.
+     */
+    bindEvents(fragment: DocumentFragment): void {
+        const eventsMap = this.eventsMap();
+        for (let eventKey in eventsMap) {
+            const [eventName, selector] = eventKey.split(":");
+
+            fragment
+                .querySelectorAll(selector)
+                .forEach(element => {
+                    element.addEventListener(eventName, eventsMap[eventKey]);
+                });
+
+        }
     }
     /* Take HTML template and append it as a child to this UserForm's parent. */
     render(): void {
         const templateElement = document.createElement("template");
         templateElement.innerHTML = this.template();
-
+        this.bindEvents(templateElement.content);
         this.parent.append(templateElement.content);
     }
 }
